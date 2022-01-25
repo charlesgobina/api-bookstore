4ac2355689a080be41a1a349e9ef8e1ba82bede3
charlesgobina <charles.gobina@gmail.com>
2022-01-25 12:51:34 +0100

    Add book component

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <div>
-    <h2>Add a book</h2>
-    <form>
-      <input type="text" placeholder="Add a Book" />
-      <input type="text" placeholder="Add a Category" />
-      <button type="button">Add</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [emptyFields, setEmptyFields] = useState(false);
+
+  const setToStore = (e) => {
+    e.preventDefault();
+    if (title === '' || author === '') {
+      setEmptyFields(true);
+    } else {
+      setEmptyFields(false);
+      const booky = {
+        id: uuidv4(),
+        title,
+        author,
+      };
+
+      dispatch(addBook(booky));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Add a book</h2>
+      <form>
+        <input value={title} type="text" onChange={(e) => setTitle(e.target.value)} placeholder="Add a Book" />
+        <input value={author} type="text" onChange={(e) => setAuthor(e.target.value)} placeholder="Add an Authur" />
+        <button onClick={(e) => setToStore(e)} type="submit">Add</button>
+      </form>
+      { emptyFields ? <small>Please fill in all required fields</small> : '' }
+    </div>
+  );
+};
 
 export default AddBook;
