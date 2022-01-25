b67c2b4c42cf3e11450ec10fa44d8aad093b90ad
charlesgobina <charles.gobina@gmail.com>
2022-01-25 12:52:00 +0100

    Book container

--- src/components/bookContainer.js
+++ src/components/bookContainer.js
@@ -1,27 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Books from './books';
 import AddBook from './addBook';
 
 const BookContainer = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Him',
-      category: 'Action',
-    },
-    {
-      id: 2,
-      title: 'Doctor Strange',
-      category: 'Sci-Fi',
-    },
-    {
-      id: 3,
-      title: 'The Four hour Work week',
-      category: 'Productivity',
-    },
-  ];
-
-  console.log(books[0]);
+  const books = useSelector((state) => state.bookReducer);
 
   return (
     <>
